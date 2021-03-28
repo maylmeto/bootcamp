ee6221baf43fde1fcc27c4d35ccef3f8671f7cd8
Mayl Meto <maylme@protonmail.com>
2021-03-28 19:54:52 +0545

    configured database

--- server.js
+++ server.js
@@ -1,28 +1,40 @@
-const express = require("express");
-const dotenv = require("dotenv");
-
-// route files
-const bootcamps = require("./routes/bootcamps");
+const express = require('express');
+const dotenv = require('dotenv');
+// const logger = require("./middleware/logger");
+const morgan = require('morgan');
+const connectDB = require('./config/db');
 
 // Load env vars
-dotenv.config({ path: "./config/config.env" });
+dotenv.config({ path: './config/config.env' });
+
+//connect to database
+connectDB();
+
+// route files
+const bootcamps = require('./routes/bootcamps');
 
 const app = express();
 
-const logger = (req, res, next) => {
-  req.hello = "Hello world";
-  console.log("Middleware ran");
-  next();
-};
-
-app.use(logger);
+// app.use(logger);
+// Dev logging middleware
+if (process.env.NODE_ENV === 'development') {
+  app.use(morgan('dev'));
+}
 
 //Mount routers
-app.use("/api/v1/bootcamps", bootcamps);
+app.use('/api/v1/bootcamps', bootcamps);
 
 const PORT = process.env.PORT || 5000;
 
-app.listen(
+const server = app.listen(
   PORT,
   console.log(`Server running in ${process.env.NODE_ENV} mode on port ${PORT}`)
 );
+
+//Handle unhandled promise rejections
+process.on('unhandledRejection', (err, promise) => {
+  console.log(`Error:${err.message}`);
+
+  // Close server & exit process
+  server.close(() => process.exit(1));
+});
